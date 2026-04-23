9a906ab4865bf44875ccb56ee044b25e97adaee2
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-04-23 10:12:31 +0700

    Update generator.js

--- generator.js
+++ generator.js
@@ -3,14 +3,18 @@ javascript.javascriptGenerator.forBlock['color_picker_custom'] = function(block)
   return ["'" + color + "'", javascript.Order.ATOMIC];
 };
 
-javascript.javascriptGenerator.forBlock['color_mixer'] = function(block) {
-  const color1 = javascript.javascriptGenerator.valueToCode(block, 'COLOR1', javascript.Order.ATOMIC);
-  const color2 = javascript.javascriptGenerator.valueToCode(block, 'COLOR2', javascript.Order.ATOMIC);
+javascript.javascriptGenerator.forBlock['color_mixer_ratio'] = function(block) {
+  const color1 = javascript.javascriptGenerator.valueToCode(block, 'COLOR1', javascript.Order.ATOMIC) || "'#ffffff'";
+  const ratio1 = block.getFieldValue('RATIO1');
+  const color2 = javascript.javascriptGenerator.valueToCode(block, 'COLOR2', javascript.Order.ATOMIC) || "'#ffffff'";
+  const ratio2 = block.getFieldValue('RATIO2');
   
-  // Default ke putih jika salah satu kosong
-  const val1 = color1 || "'#ffffff'";
-  const val2 = color2 || "'#ffffff'";
-  
-  const code = `mixColors(${val1}, ${val2})`;
+  const code = `mixColorsRatio(${color1}, ${ratio1}, ${color2}, ${ratio2})`;
+  return [code, javascript.Order.FUNCTION_CALL];
+};
+
+javascript.javascriptGenerator.forBlock['get_stored_color'] = function(block) {
+  const id = block.getFieldValue('STORAGE_ID');
+  const code = `getStorageColor(${id})`;
   return [code, javascript.Order.FUNCTION_CALL];
 };
