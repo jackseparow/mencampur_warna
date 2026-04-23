fb0f43a61e5049f1e789dab2e3ce6b696d45ff2c
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-04-23 10:03:37 +0700

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,16 +1,16 @@
-// Generator untuk blok pemilih warna
 javascript.javascriptGenerator.forBlock['color_picker_custom'] = function(block) {
   const color = block.getFieldValue('COL');
-  // Kita bungkus dengan tanda kutip agar dibaca sebagai string di JavaScript
   return ["'" + color + "'", javascript.Order.ATOMIC];
 };
 
-// Generator untuk blok pencampur
 javascript.javascriptGenerator.forBlock['color_mixer'] = function(block) {
   const color1 = javascript.javascriptGenerator.valueToCode(block, 'COLOR1', javascript.Order.ATOMIC);
   const color2 = javascript.javascriptGenerator.valueToCode(block, 'COLOR2', javascript.Order.ATOMIC);
   
-  // Menghasilkan pemanggilan fungsi mixColors('#warna1', '#warna2')
-  const code = `mixColors(${color1 || "'#ffffff'"}, ${color2 || "'#ffffff'"})`;
+  // Default ke putih jika salah satu kosong
+  const val1 = color1 || "'#ffffff'";
+  const val2 = color2 || "'#ffffff'";
+  
+  const code = `mixColors(${val1}, ${val2})`;
   return [code, javascript.Order.FUNCTION_CALL];
 };
