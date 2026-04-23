e0e54ec3fa39196c76235ed6810e8ea404be98e2
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-04-23 10:17:17 +0700

    Update generator.js

--- generator.js
+++ generator.js
@@ -5,9 +5,9 @@ javascript.javascriptGenerator.forBlock['color_picker_custom'] = function(block)
 
 javascript.javascriptGenerator.forBlock['color_mixer_ratio'] = function(block) {
   const color1 = javascript.javascriptGenerator.valueToCode(block, 'COLOR1', javascript.Order.ATOMIC) || "'#ffffff'";
-  const ratio1 = block.getFieldValue('RATIO1');
+  const ratio1 = block.getFieldValue('RATIO1') || 1;
   const color2 = javascript.javascriptGenerator.valueToCode(block, 'COLOR2', javascript.Order.ATOMIC) || "'#ffffff'";
-  const ratio2 = block.getFieldValue('RATIO2');
+  const ratio2 = block.getFieldValue('RATIO2') || 1;
   
   const code = `mixColorsRatio(${color1}, ${ratio1}, ${color2}, ${ratio2})`;
   return [code, javascript.Order.FUNCTION_CALL];
@@ -15,6 +15,5 @@ javascript.javascriptGenerator.forBlock['color_mixer_ratio'] = function(block) {
 
 javascript.javascriptGenerator.forBlock['get_stored_color'] = function(block) {
   const id = block.getFieldValue('STORAGE_ID');
-  const code = `getStorageColor(${id})`;
-  return [code, javascript.Order.FUNCTION_CALL];
+  return [`getStorageColor(${id})`, javascript.Order.FUNCTION_CALL];
 };
